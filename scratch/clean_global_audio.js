d21daecc3b925b8e1e8823ff1a2e9731bff5ad57
nilobonca <juan.x.vip@gmail.com>
2026-06-29 13:07:18 -0300

    Exclusão de arquivos desnecessarios de automação de correções

--- scratch/clean_global_audio.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let contentGlobal = fs.readFileSync('src/components/GlobalAudioMenu/index.tsx', 'utf8');
-contentGlobal = contentGlobal.replace(/\s*is3DEnabled=\{is3DEnabled\}/g, '');
-
-fs.writeFileSync('src/components/GlobalAudioMenu/index.tsx', contentGlobal, 'utf8');
-console.log('Cleaned GlobalAudioMenu');
--- scratch/fix_store_import.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/GlobalAudioMenu/index.tsx', 'utf8');
-
-content = content.replace("import { useCanvasStore }", "import { useCanvasGlobalStore }");
-fs.writeFileSync('src/components/GlobalAudioMenu/index.tsx', content, 'utf8');
-console.log('Fixed import!');
